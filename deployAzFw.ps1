28dae0fd6bbd02ac32cfa34b5a4c3d2571f2e44c
elbromed <elbromed@gmail.com>
2020-03-25 15:05:50 -0400

    http://srvtfs01:8080/tfs/Infrastructure/_git/Azure-test

--- deployAzFw.ps1
+++ deployAzFw.ps1
@@ -9,4 +9,4 @@ $Namedeployment="AzFwEnvironment"
 New-AzResourceGroup -Name $rsg -Location $Location 
 New-AzResourceGroupDeployment -Name $Namedeployment -ResourceGroupName $rsg -TemplateFile $templateFile -TemplateParameterFile $parameterFile -Verbose
 
-#az group deployment validate --resource-group RG-HUB --template-file .\GumHUB01.template.json --parameters .\GumHUB01.parameters.json
+#az group deployment validate --resource-group RG-HUB --template-file .\GumHUB02.template.json --parameters .\GumHUB02.parameters.json
